963c04525977668c56d9c1e724db33ff5b310908
Ali Razzak Hussien <ahmedrazzakalamry@icloud.com>
2026-08-22 22:47:52 +0300

    Create dues_data.js

---
+++ dues_data.js
@@ -0,0 +1,6 @@
+const duesInvoicesData = [
+    { id: 1, wholesaler: "مذخر الحياة", date: "2026-08-01", amount: 150000, validDays: 30 },
+    { id: 2, wholesaler: "مذخر الشفاء", date: "2026-08-15", amount: 350000, validDays: 30 },
+    { id: 3, wholesaler: "مذخر الحياة", date: "2026-08-20", amount: 120000, validDays: 15 },
+    { id: 4, wholesaler: "مذخر بغداد", date: "2026-08-25", amount: 500000, validDays: 30 }
+];
